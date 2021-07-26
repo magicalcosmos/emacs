1631530c437bf56c01abe288413258d8671a2aa3
brodyliao <brodyliao@msn.com>
2021-07-27 07:40:02 +0800

    add new config

--- my-init.el
+++ my-init.el
@@ -1210,16 +1210,18 @@
  :bind(("<M-up>" . drag-stuff-up)
  ("<M-down>" . drag-stuff-updown)))
 
-;;  (use-package ivy-posframe
-;;    :ensure t)
-;;;; Different command can use different display function.
-;;(setq ivy-posframe-display-functions-alist
-;;      '((swiper          . ivy-posframe-display-at-window-center)
-;;        (complete-symbol . ivy-posframe-display-at-window-center)
-;;        (counsel-M-x     . ivy-posframe-display-at-window-center)
-;;        (counsel-find-file     . ivy-posframe-display-at-window-center)
-;;        (t               . ivy-posframe-display-at-window-center)))
-;;(ivy-posframe-mode 1)
+(use-package ivy-posframe
+    :ensure t)
+  ;; Different command can use different display function.
+  (setq ivy-posframe-display-functions-alist
+    '((swiper          . ivy-posframe-display-at-window-center)
+      (complete-symbol . ivy-posframe-display-at-window-center)
+      (counsel-M-x     . ivy-posframe-display-at-window-center)
+      (counsel-find-file     . ivy-posframe-display-at-window-center)
+      (t               . ivy-posframe-display-at-window-center)))
+  (ivy-posframe-mode 1)
+(setq ivy-posframe-height-alist '((swiper . 20)
+                                  (t      . 20)))
 
 (use-package eshell-toggle
   :ensure t
