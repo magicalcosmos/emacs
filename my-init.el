3e10d9036e6fc30bbbaadd2f920a3ff8f52b21ea
brodyliao <brodyliao@msn.com>
2021-08-13 07:26:25 +0800

    git add undo tree

--- my-init.el
+++ my-init.el
@@ -50,50 +50,53 @@
     (persp-mode)))
 
 (defun bl/evil-hook ()
-  (dolist (mode '(custom-mode
-                  eshell-mode
-                  git-rebase-mode
-                  erc-mode
-                  circe-server-mode
-                  circe-chat-mode
-                  circe-query-mode
-                  sauron-mode
-                  term-mode))
-  (add-to-list 'evil-emacs-state-modes mode)))
-(defun bl/dont-arrow-me-bro ()
-(interactive)
-(message "Arrow keys are bad, you know?"))
-        (use-package evil
-          :init
-          (setq evil-want-fine-undo t)
-          (setq evil-want-keybinding nil)
-          (setq evil-want-integration t)
-          (setq evil-want-C-u-scroll t)
-          (setq evil-want-C-i-jump nil)
-          (setq evil-respect-visual-line-mode t)
-          :config
-          (add-hook 'evil-mode-hook 'bl/evil-hook)
-          (evil-mode 1)
-          (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-          (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+      (dolist (mode '(custom-mode
+                      eshell-mode
+                      git-rebase-mode
+                      erc-mode
+                      circe-server-mode
+                      circe-chat-mode
+                      circe-query-mode
+                      sauron-mode
+                      term-mode))
+      (add-to-list 'evil-emacs-state-modes mode)))
+    (defun bl/dont-arrow-me-bro ()
+    (interactive)
+    (message "Arrow keys are bad, you know?"))
+            (use-package evil
+              :init
+              (setq evil-want-fine-undo t)
+              (setq evil-want-keybinding nil)
+              (setq evil-want-integration t)
+              (setq evil-want-C-u-scroll t)
+              (setq evil-want-C-i-jump nil)
+              (setq evil-respect-visual-line-mode t)
+              :config
+              (add-hook 'evil-mode-hook 'bl/evil-hook)
+              (evil-mode 1)
+              (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+              (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-          ;; Use visual line motions even outside of visual-line-mode buffers
-          (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-          (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+              ;; Use visual line motions even outside of visual-line-mode buffers
+              (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+              (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
-          (unless bl/is-termux
-            ;; Disable arrow keys in normal and visual modes
-            (define-key evil-normal-state-map (kbd "<left>") 'bl/dont-arrow-me-bro)
-            (define-key evil-normal-state-map (kbd "<right>") 'bl/dont-arrow-me-bro)
-            (define-key evil-normal-state-map (kbd "<down>") 'bl/dont-arrow-me-bro)
-            (define-key evil-normal-state-map (kbd "<up>") 'bl/dont-arrow-me-bro)
-            (evil-global-set-key 'motion (kbd "<left>") 'bl/dont-arrow-me-bro)
-            (evil-global-set-key 'motion (kbd "<right>") 'bl/dont-arrow-me-bro)
-            (evil-global-set-key 'motion (kbd "<down>") 'bl/dont-arrow-me-bro)
-            (evil-global-set-key 'motion (kbd "<up>") 'bl/dont-arrow-me-bro))
+              (unless bl/is-termux
+                ;; Disable arrow keys in normal and visual modes
+                (define-key evil-normal-state-map (kbd "<left>") 'bl/dont-arrow-me-bro)
+                (define-key evil-normal-state-map (kbd "<right>") 'bl/dont-arrow-me-bro)
+                (define-key evil-normal-state-map (kbd "<down>") 'bl/dont-arrow-me-bro)
+                (define-key evil-normal-state-map (kbd "<up>") 'bl/dont-arrow-me-bro)
+                (evil-global-set-key 'motion (kbd "<left>") 'bl/dont-arrow-me-bro)
+                (evil-global-set-key 'motion (kbd "<right>") 'bl/dont-arrow-me-bro)
+                (evil-global-set-key 'motion (kbd "<down>") 'bl/dont-arrow-me-bro)
+                (evil-global-set-key 'motion (kbd "<up>") 'bl/dont-arrow-me-bro))
 
-          (evil-set-initial-state 'messages-buffer-mode 'normal)
-          (evil-set-initial-state 'dashboard-mode 'normal))
+              (evil-set-initial-state 'messages-buffer-mode 'normal)
+              (evil-set-initial-state 'dashboard-mode 'normal))
+
+(global-undo-tree-mode)
+(evil-set-undo-system 'undo-tree)
 
 (require 'org-tempo)
 
